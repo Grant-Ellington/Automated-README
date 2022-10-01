6a34877bafed43c66e4dfe35be4851aed6d0f64c
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-01 17:00:17 -0500

    added generative readme functionality

---
+++ README.md
@@ -0,0 +1,26 @@
+## Table of Contents
+    * [NOrma and Eleaine Project](*NOrma and Eleaine Project)
+    * [Introduction](*Introduction)
+    * [Technologies](*Technologies)
+    * [Set-up](*Set-up)
+    * [What-was-accomplished](*What-was-accomplished)
+    
+    #NOrma and Eleaine Project 
+    ###Maagie
+    ## Introduction
+    
+    asdflaksdjf;alksdjfa;lsdkfja;slkdfj
+    
+    ## Techonologies
+    Project is created with:
+    * cute
+    * revit
+    * undefined
+    ## Set-up
+
+    
+    ## What was accomplished
+    * Using bootrap- created a css front end with flexiblen design.
+    * I continously up-dating clock.
+    * A calendar which udates background color based on time of loading
+    * A claendar spots which incorporates local storage to store important events.
--- index.js
+++ index.js
@@ -2,55 +2,86 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// // TODO: Create an array of questions for user input
-// const questions = ['What is your name?',
-//                     'What is your project title?',
-//                     'Eneter a short subnopsis of you projecct?',
-//                     'What techonologyis one rechnology that you used?',
+// TODO: Create an array of questions for user input
+const questions = ['What is your name?',
+                    'What is your project title?',
+                    'Eneter a short synoqsis of you projecct?',
+                    'What is one technology that you used?',
+                    'What is another technology you used?',
+                    'What is another technology you used?'
+                    ];
 
-//                     'What is another technology you used?',
-//                     'What is another technology you used?'
-//                     ];
+// TODO: Create a function to write README file
+const writeToFile = ({name, title, synopsis, tech1, tech2, tech3}) =>
+    `## Table of Contents
+    * [${title}](*${title})
+    * [Introduction](*Introduction)
+    * [Technologies](*Technologies)
+    * [Set-up](*Set-up)
+    * [What-was-accomplished](*What-was-accomplished)
+    
+    #${title} 
+    ###${name}
+    ## Introduction
+    
+    ${synopsis}
+    
+    ## Techonologies
+    Project is created with:
+    * ${tech1}
+    * ${tech2}
+    * ${tech3}
+    ## Set-up
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+    
+    ## What was accomplished
+    * Using bootrap- created a css front end with flexiblen design.
+    * I continously up-dating clock.
+    * A calendar which udates background color based on time of loading
+    * A claendar spots which incorporates local storage to store important events.`
 
-// // TODO: Create a function to initialize app
-function init() {}
+// TODO: Create a function to initialize app
+function init() {inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: questions[0],
+        },
+        {
+        type: 'input',
+        name: 'title',
+        message: questions[1],
+        },
+        {
+        type: 'input',
+        name: 'synopsis',
+        message: questions[2],
+        },
+        {
+        type: 'input',
+        name: 'tech1',
+        message: questions[3],
+        },
+        {
+        type: 'input',
+        name: 'tech2',
+        message: questions[4],
+        },
+        {
+        type: 'input',
+        name: 'tech5',
+        message: questions[5],
+        },
+    ])
+    .then((answers) => {
+        const readMeCont = writeToFile(answers);
 
-inquirer
-.prompt([
-    {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?',
-    },
-    {
-    type: 'input',
-    name: 'location',
-    message: 'Where are you from?',
-    },
-    {
-    type: 'input',
-    name: 'hobby',
-    message: 'What is your favorite hobby?',
-    },
-    {
-    type: 'input',
-    name: 'food',
-    message: 'What is your favorite food?',
-    },
-    {
-    type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub Username',
-    },
-    {
-    type: 'input',
-    name: 'linkedin',
-    message: 'Enter your LinkedIn URL.',
-    },
-])
+        fs.writeFile('README.md', readMeCont, (err)=> 
+        err ? console.log('err') : console.log('success'))
+    })
+}
+
 
 // Function call to initialize app
 init();
