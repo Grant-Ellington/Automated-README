84bbd0085c3abe10f696cc977f16043788fd6834
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-02 21:03:17 -0500

    made changes alog the line;; of instructionss, need to find out how link file functionality works

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,59 @@
-const index = require('./index')
 
-console.log(index.license)
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    switch(license) {
+        case 'MIT Liscense':
+            let badge = 
+            break;
+        case 'GNU GPLv3':
+            let badge = 
+            break;
+        case '':
+            let badge = 
+
+
+    }
+    return badge
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    switch(license) {
+        case 'MIT Liscense':
+            let link = 
+            break;
+        case 'GNU GPLv3':
+            let link = 
+            break;
+        case '':
+            let link = 
+
+
+    }
+    return link
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, badge, link) {
+    switch(license) {
+        case 'MIT Liscense':
+            let section = 
+            break;
+        case 'GNU GPLv3':
+            let section = 
+            break;
+        case '':
+            let section = 
+
+
+    }
+    return section
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
