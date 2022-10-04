9aee483974de516a17fc91051a19a69313275ff6
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-04 10:18:06 -0500

    saving for questions

--- index.js
+++ index.js
@@ -1,162 +1,120 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+//markdown js file import
+const generateMarkdown = require('./utils/generateMarkdown')
+//checks the input for valididity of the input of each question
+function validateInput(value) {
+    if(value !='') {
+        return true
+    } else {
+        return "please enter a  valid input"
+    }
+}
 
 // TODO: Create an array of questions for user input
-const questions = ['What is your name?',
-                    'What is your project title?',
-                    'Eneter a short synoqsis of you projecct?',
-                    'What is one technology that you used?',
-                    'What is another technology you used?',
-                    'What is another technology you used?',
-                    'How do you install your project?',
-                    'What are your Usage Insrunctions?',
-                    'Who are your constribution guidleines?',
-                    'What are your test?',
-                    'What license do you need?',
-                    ];
-const licInfo = {
-    MIT:['https://img.shields.io/badge/License-MIT-yellow.svg', 'https://opensource.org/licenses/MIT'],
-    GNU: ['License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg', 'https://www.gnu.org/licenses/gpl-3.0'],
-    none: ['','']
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'synopsis',
+        message: 'Eneter a short synopsis of you projecct?',
+    },
+    {
+        type: 'input',
+        name: 'tech1',
+        message: 'What is one technology that you used?',
+    },
+    {
+        type: 'input',
+        name: 'tech2',
+        message: 'What is another technology you used?',
+    },
+    {
+        type: 'input',
+        name: 'tech3',
+        message: 'What is another technology you used?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?',
+        },
+    {
+        type: 'input',
+        name: 'usage',
+        message:'What are your Usage Insrunctions?',
+        },
+    {
+        type: 'input',
+        name: 'contGuide',
+        message: 'Who are your constribution guidleines?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are your test?'  
+        
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license do you need?',
+        choices: [
+            'MIT',
+            'GNU GLPv3',
+            '',
+        ]
+    },
+];
+// const licInfo = {
+//     MIT:['https://img.shields.io/badge/License-MIT-yellow.svg', 'https://opensource.org/licenses/MIT'],
+//     GNU: ['License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg', 'https://www.gnu.org/licenses/gpl-3.0'],
+//     none: ['','']
 
-}
+// }
 
-switch(license) {
-    case 'MIT Liscense':
-        var img = licInfo.MIT[0]
-        var link = licInfo.MIT[1]
-        break;
-    case 'GNU GPLv3':
-        var img = licInfo.GNU[0]
-        var link = licInfo.GNU[0]
-        break;
-    case '':
-        var img = ''
-        var link = ''
-        break;
-}
+// switch(license) {
+//     case 'MIT Liscense':
+//         var img = licInfo.MIT[0]
+//         var link = licInfo.MIT[1]
+//         break;
+//     case 'GNU GPLv3':
+//         var img = licInfo.GNU[0]
+//         var link = licInfo.GNU[0]
+//         break;
+//     case '':
+//         var img = ''
+//         var link = ''
+//         break;
+// }
 
 // TODO: Create a function to write README file
-const writeToFile = ({name, title, synopsis, tech1, tech2, tech3, installation, usage,contGuide, test, license, img, link}) =>
-    `## Table of Contents
-    * *[License](*License)
-    * [${title}](*${title})
-    * [Description](*Description)
-    * [Installation](*Installation)
-    * [Usage Information](*Usage Information)
-    * [Contribution Guidelines](*Contribution Guidlines)
-    * [Test](*Test)
-    * [Technologies](*Technologies)
-  
-    ##License
-
-    [![License: ${license}](${img})](${link})
-    
-
-    #${title} 
-    ###${name}
-    ## Description
-    
-    ${synopsis}
-
-    ##Installation
-
-    ${installation}
-
-    ##Usage Information
-
-    ${usage}
-
-    ##Contribution Guidlines
-
-    ${contGuide}
-
-    ##Test
-
-    ${test}
-
-    
-    ## Techonologies
-    Project is created with:
-    * ${tech1}
-    * ${tech2}
-    * ${tech3}
-    ## Set-up
-
-    
-    ## What was accomplished`
+const writeToFile = (answers) => {
+    console.log(answers)
+    fs.writeFile('README.md', generateMarkdown(answers), (err)=> 
+        err ? console.log('err') : console.log('success'))
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-        .prompt([
-            {
-            type: 'input',
-            name: 'name',
-            message: questions[0],
-            },
-            {
-            type: 'input',
-            name: 'title',
-            message: questions[1],
-            },
-            {
-            type: 'input',
-            name: 'synopsis',
-            message: questions[2],
-            },
-            {
-            type: 'input',
-            name: 'tech1',
-            message: questions[3],
-            },
-            {
-            type: 'input',
-            name: 'tech2',
-            message: questions[4],
-            },
-            {
-            type: 'input',
-            name: 'tech5',
-            message: questions[5],
-            },
-            {
-            type: 'input',
-            name: 'installation',
-            message: questions[6],
-            },
-            {
-            type: 'input',
-            name: 'usage',
-            message: questions[7],
-            },
-            {
-            type: 'input',
-            name: 'contGuide',
-            message: questions[8],
-            },
-            {
-            type: 'input',
-            name: 'test',
-            message: questions[9],
-            },
-            {
-            type: 'list',
-            name: 'license',
-            message: questions[10],
-            choices: ['MIT Liscense', 'GNU GPLv3']
-            },
-    ])
-    .then((answers) => {
-        const readMeCont = writeToFile(answers);
-        const license = answers.license
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            writeToFile(answers)
+        })
 
         
-
-        fs.writeFile('README.md', readMeCont, (err)=> 
-        err ? console.log('err') : console.log('success'))
-    })
 }
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,67 +1,67 @@
- const index = require('./index')
 
- console.log(license)
 
 // // TODO: Create a function that returns a license badge based on which license is passed in
 // // If there is no license, return an empty string
 
-// function renderLicenseBadge(license) {
-//     switch(license) {
-//         case 'MIT Liscense':
-//             let badge = 
-//             break;
-//         case 'GNU GPLv3':
-//             let badge = 
-//             break;
-//         case '':
-//             let badge = 
-
-
-//     }
-//     return badge
-// }
+// function renderLicenseBadge(data) {}
 
 // // TODO: Create a function that returns the license link
 // // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//     switch(license) {
-//         case 'MIT Liscense':
-//             let link = 
-//             break;
-//         case 'GNU GPLv3':
-//             let link = 
-//             break;
-//         case '':
-//             let link = 
+// function renderLicenseLink(data) {}
 
-
-//     }
-//     return link
-// }
 
 // // TODO: Create a function that returns the license section of README
 // // If there is no license, return an empty string
-// function renderLicenseSection(license, badge, link) {
-//     switch(license) {
-//         case 'MIT Liscense':
-//             let section = 
-//             break;
-//         case 'GNU GPLv3':
-//             let section = 
-//             break;
-//         case '':
-//             let section = 
+// function renderLicenseSection(data) {}
 
 
-//     }
-//     return section
-// }
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(name, title, synopsis, tech1, tech2, tech3, installation, usage, contGuide, test, license){
+    `## Table of Contents
+    * *[License](*License)
+    * [Title](*Title)
+    * [Description](*Description)
+    * [Installation](*Installation)
+    * [Usage Information](*Usage Information)
+    * [Contribution Guidelines](*Contribution Guidlines)
+    * [Test](*Test)
+    * [Technologies](*Technologies)
+  
+    ##License
+    
 
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
+    #${title} 
+    ###${name}
+    ## Description
+    
+    ${synopsis}
 
-// `;
-// }
+    ##Installation
 
-// module.exports = generateMarkdown;
+    ${installation}
+
+    ##Usage Information
+
+    ${usage}
+
+    ##Contribution Guidlines
+
+    ${contGuide}
+
+    ##Test
+
+    ${test}
+
+    
+    ## Techonologies
+    Project is created with:
+    * ${tech1}
+    * ${tech2}
+    * ${tech3}
+    ## Set-up
+
+    
+    ## What was accomplished`
+}
+
+module.exports = generateMarkdown;
